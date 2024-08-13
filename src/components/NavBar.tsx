09a8bd120ecb28608b0f6d1083206f6b8a05eeb5
Shiv Pratap <sp09singhwaghel@gmail.com>
2024-08-13 11:22:28 +0530

    basic nav done

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,38 @@
+import { FaSearch } from "react-icons/fa";
+import { CiBellOn } from "react-icons/ci";
+import { FaRegUserCircle } from "react-icons/fa";
+import { IoIosArrowDown } from "react-icons/io";
+
 export default function NavBar() {
   return (
     <div className="flex justify-between items-center  py-3 px-5 font-sans">
+        {/* left */}
       <div>
         Home &lt; <span className="font-bold text-blue-800">Dashboardv2</span>
       </div>
-      <div>
-        <div className="w-full max-w-md mx-auto">
-          <input
-            type="text"
-            className="w-full px-2  border-2 border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500 text-lg text-gray-700"
-            placeholder="Search anything"
-            // value={value}
-            // onChange={onchange}
-          />
+
+      {/* middle */}
+
+      <div className="w-full max-w-md mx-auto relative">
+        <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+
+        <input
+          type="text"
+          className="w-full px-2   pl-12 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500 text-lg text-gray-700"
+          placeholder="Search anything"
+        />
+      </div>
+
+      {/* top */}
+      <div className="flex justify-between gap-14 items-center ">
+        <IoIosArrowDown className="cursor-pointer" />
+
+        <CiBellOn className="text-2xl cursor-pointer" />
+        <div className="flex justify-center gap-2 items-center border-2 px-4 cursor-pointer py-1 border-blue-800 rounded">
+          <FaRegUserCircle className="text-blue-800" />
+          Login
         </div>
       </div>
-      <div>icon</div>
     </div>
   );
 }
